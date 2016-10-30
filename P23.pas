ed5d13090762891379aac732d07a37b4e207ab3f
anvas <andrey.4mail@gmail.com>
2016-10-30 11:24:33 +0300

    Initial commit

---
+++ P23.pas
@@ -0,0 +1,22 @@
+(*
+ * Project: P23
+ * User: Василевский
+ * Date: 28.10.2016
+ *)
+program P23;
+var
+    x,y,days:Integer;
+    i:Real;
+begin
+    ReadLn(x);
+    ReadLn(y);
+    i:=x;
+    days:=1;
+    while i<=y do
+    begin
+        i:=i+(i*0.1);
+        days:=days+1;
+    end;
+    WriteLn(days);
+
+end.
